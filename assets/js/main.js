32f9e032c31c4adf442764e791dec105d56d67d5
Henrique Griepp <henriquegriepp@DESKTOP-SU6BTHJ>
2024-01-04 19:21:08 -0300

    Implement parallel request handling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,25 @@
+function convertPokemonTypesToLi(pokemonTypes) {
+  return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`);
+}
 
 function convertePokemonToLi(pokemon) {
   return `
-  <li class="pokemon">
-  <span class="number">#001</span>
-  <span class="name">${pokemon.name}</span>
+    <li class="pokemon">
+      <span class="number">#${pokemon.order}</span>
+      <span class="name">${pokemon.name}</span>
 
-  <div class="detail">
-    <ol class="types">
-      <li class="type">grass</li>
-      <li class="type">poison</li>
-    </ol>
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}" srcset="">          
-  </div>
-  </li>`;
+      <div class="detail">
+        <ol class="types">
+          ${convertPokemonTypesToLi(pokemon.types).join("")}
+        </ol>
+        <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">          
+      </div>
+    </li>
+  `;
 }
 
 const pokemonList = document.getElementById(`pokemonList`);
 
 pokeApi.getPokemons().then((pokemons = []) => {
-  pokemonList.innerHTML += pokemons.map(convertePokemonToLi).join(``)
+  pokemonList.innerHTML += pokemons.map(convertePokemonToLi).join(``);
 });
